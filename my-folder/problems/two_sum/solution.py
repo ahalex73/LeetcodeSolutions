7ffca603858e4b55e15eaa3ad3e665fe06e1b014
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-04-11 14:57:02 +0000

    Sync LeetCode submission - Two Sum (python)

---
+++ my-folder/problems/two_sum/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {} # Key: value we saw in nums[i], value: index where we found it
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+
+        return []
+
