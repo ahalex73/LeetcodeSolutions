3dda97c80cac23d9a31236ab01345591438a96a6
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-03 04:31:14 +0000

    Sync LeetCode submission - Richest Customer Wealth - Runtime - 0 ms (100.00%), Memory - 42.6 MB (94.80%)

--- my-folder/1791-richest-customer-wealth/solution.java
+++ my-folder/1791-richest-customer-wealth/solution.java
@@ -1,19 +1,36 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
         int max_wealth = 0;
-
-        for(int i = 0; i < accounts.length; i++){
-            // each customer
+        for(int[] customer: accounts){
             int curr_wealth = 0;
 
-            for(int j = 0; j < accounts[i].length; j++){
-                // each customer account
-                curr_wealth += accounts[i][j];
+            for(int bank : customer){
+                curr_wealth += bank;
             }
 
             max_wealth = java.lang.Math.max(curr_wealth, max_wealth);
         }
-    
-    return max_wealth;
+
+        return max_wealth;
     }
 }
+
+// class Solution {
+//     public int maximumWealth(int[][] accounts) {
+//         int max_wealth = 0;
+
+//         for(int i = 0; i < accounts.length; i++){
+//             // each customer
+//             int curr_wealth = 0;
+
+//             for(int j = 0; j < accounts[i].length; j++){
+//                 // each customer account
+//                 curr_wealth += accounts[i][j];
+//             }
+
+//             max_wealth = java.lang.Math.max(curr_wealth, max_wealth);
+//         }
+    
+//     return max_wealth;
+//     }
+// }
