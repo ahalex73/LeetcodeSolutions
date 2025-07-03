ee731bea00badcea4d45fcc3fbc50a03c432cd14
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-03 05:03:52 +0000

    Sync LeetCode submission - Number of Steps to Reduce a Number to Zero - Runtime - 0 ms (100.00%), Memory - 40.4 MB (68.74%)

--- my-folder/1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
+++ my-folder/1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
@@ -1,21 +1,45 @@
 class Solution {
+    // BitWise solution
     public int numberOfSteps(int num) {
         int steps = 0;
+        int bitMask = 1;
 
         while(num > 0){
-            boolean isEven = num % 2 == 0;
-
-            if (isEven){
-                num = num / 2;
+            if((num & bitMask) == 0){
+                num = num >> 1;
             }
-
             else{
                 num -= 1;
             }
 
+
             steps += 1;
         }
+        
 
         return steps;
     }
 }
+
+
+// class Solution {
+//     public int numberOfSteps(int num) {
+//         int steps = 0;
+
+//         while(num > 0){
+//             boolean isEven = num % 2 == 0;
+
+//             if (isEven){
+//                 num = num / 2;
+//             }
+
+//             else{
+//                 num -= 1;
+//             }
+
+//             steps += 1;
+//         }
+
+//         return steps;
+//     }
+// }
