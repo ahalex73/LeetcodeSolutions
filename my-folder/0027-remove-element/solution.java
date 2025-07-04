d20749f90f9810ab5123bded788d792f0c285325
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-04 03:22:46 +0000

    Sync LeetCode submission - Remove Element - Runtime - 0 ms (100.00%), Memory - 41.9 MB (67.06%)

---
+++ my-folder/0027-remove-element/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int lp = 0;
+        int i = 0;
+
+        while(lp < nums.length){
+            if(nums[lp] != val){
+                nums[i++] = nums[lp++];
+            }
+
+            else{
+                lp++;
+            }
+        }
+        return i;
+    }
+}
+
