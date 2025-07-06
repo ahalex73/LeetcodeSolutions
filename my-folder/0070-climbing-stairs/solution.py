d33477a2fd983c76e130b4b765aa684d37007a02
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-06 19:27:44 +0000

    Sync LeetCode submission - Climbing Stairs - Runtime - 0 ms (100.00%), Memory - 17.9 MB (7.95%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -4,13 +4,13 @@ class Solution:
         if n == 1: return 1
         if n == 2: return 2
 
-        dp_memo = [0]*(n + 1)
-        dp_memo[1] = 1
-        dp_memo[2] = 2
+        dp = [0]*(n + 1)
+        dp[1] = 1
+        dp[2] = 2
 
         for i in range(3, n + 1):
-            dp_memo[i] = dp_memo[i - 1] + dp_memo[i - 2]
+            dp[i] = dp[i - 1] + dp[i - 2]
         
-        print(dp_memo)
-        return dp_memo[n]
+        print(dp)
+        return dp[n]
 
