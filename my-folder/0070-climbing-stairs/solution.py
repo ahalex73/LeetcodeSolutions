90f17e187659bcf656d512b898e59b7eaee3923c
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-06 19:51:17 +0000

    Sync LeetCode submission - Climbing Stairs - Runtime - 0 ms (100.00%), Memory - 18 MB (7.95%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -9,27 +9,27 @@ class Solution:
         if n == 2: return 2
 
         # UNOPTIMAL SPACE COMPLEXITY considering we know we only have two subproblems possible
-        # dp = [0]*(n + 1) # Stores solutions to sub-problems in dynamic programming
-        # dp[1] = 1
-        # dp[2] = 2
+        dp = [0]*(n + 1) # Stores solutions to sub-problems in dynamic programming
+        dp[1] = 1
+        dp[2] = 2
 
-        # for i in range(3, n + 1):
-        #     dp[i] = dp[i - 1] + dp[i - 2]
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
 
-        # return dp[n]
+        return dp[n]
 
         # OPTIMAL SPACE COMPLEXITY #
         # would result when we just realize we already know the answers to the subproblems in this case
         # n == 0, n == 1, and n == 2 wouldn't need to be stored in a table, they can just be two variables
-        first = 1   # dp[i - 2]
-        second = 2  # dp[i - 1]
+        # first = 1   # dp[i - 2]
+        # second = 2  # dp[i - 1]
 
-        for i in range(3, n + 1):
-            third = first + second
-            first = second
-            second = third
+        # for i in range(3, n + 1):
+        #     third = first + second
+        #     first = second
+        #     second = third
 
-        return second
+        # return second
         
 
 
