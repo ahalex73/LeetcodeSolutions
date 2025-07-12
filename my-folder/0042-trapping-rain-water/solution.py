0b58df07b77390a46c1531bc654e9c293329200f
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-12 23:37:00 +0000

    Sync LeetCode submission - Trapping Rain Water - Runtime - 11 ms (71.69%), Memory - 19.3 MB (68.66%)

---
+++ my-folder/0042-trapping-rain-water/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Time: O(n)
+        # Space: O(1)
+
+        if not height:
+            return 0
+
+        l = 0
+        r = len(height) - 1
+        leftMax = height[l]
+        rightMax = height[r]
+        res = 0
+
+        while(l < r):
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
