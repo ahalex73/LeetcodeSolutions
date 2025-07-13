491a15ded6178aa0f732feb3264da0b00be51db0
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-13 17:28:38 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree - Runtime - 0 ms (100.00%), Memory - 19.2 MB (29.79%)

--- my-folder/0104-maximum-depth-of-binary-tree/solution.py
+++ my-folder/0104-maximum-depth-of-binary-tree/solution.py
@@ -4,12 +4,32 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
     # Time: O(n) Number of nodes in tree
     # Space: O(h) height of binary tree
 
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # Time: O(n) - numbers of nodes
+        # Space: O(w) - width of the tree
         if not root:
             return 0
-        
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+        q = deque()
+        q.append(root)
+        depth = 0
+
+        while q:
+            depth += 1
+
+            for _ in range(len(q)):
+                # Pop the parent node
+                node = q.popleft() 
+                # Append children nodes
+                if(node.left):
+                    q.append(node.left)
+                if(node.right):
+                    q.append(node.right)
+            
+        return depth
