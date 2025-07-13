2790611b73dd096f61d335a45da6f272005f32a0
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-13 03:12:40 +0000

    Sync LeetCode submission - Process String with Special Operations II - Runtime - 351 ms (100.00%), Memory - 18 MB (100.00%)

---
+++ my-folder/3939-process-string-with-special-operations-ii/solution.py
@@ -0,0 +1,37 @@
+class Solution:
+    def processStr(self, s: str, k: int) -> str:
+        length = 0
+        for ch in s:
+            if ch.islower():
+                length += 1
+            elif ch == '*':
+                if length > 0:
+                    length -= 1
+            elif ch == '#':
+                length *= 2
+            elif ch == '%':
+                pass
+        
+        if k >= length:
+            return '.'
+
+        for i in reversed(range(len(s))):
+            ch = s[i]
+            if ch.islower():
+                if k == length - 1:
+                    return ch
+                length -= 1
+            elif ch == '*':
+                if length < 10**15:
+                    length += 1
+                if k == length - 1:
+                    pass
+            elif ch == '#':
+                length //= 2
+                if k >= length:
+                    k -= length
+            elif ch == '%':
+                k = length - 1 - k
+
+        return '.'
+
