f3c5b9cca67585a05f7bd8f3b313ef0eb94a6523
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-13 13:56:15 +0000

    Sync LeetCode submission - Maximum Matching of Players With Trainers - Runtime - 83 ms (32.19%), Memory - 33.6 MB (81.92%)

--- my-folder/2497-maximum-matching-of-players-with-trainers/solution.py
+++ my-folder/2497-maximum-matching-of-players-with-trainers/solution.py
@@ -2,18 +2,13 @@ class Solution:
     def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
         # Time: O(n)
         # Space: O(1)
-        
         players.sort()
         trainers.sort()
-
-        player = 0
-        trainer = 0
-        matches = 0
+        player, trainer = 0, 0
         
         while(player < len(players) and trainer < len(trainers)):
             if players[player] <= trainers[trainer]:
                 player += 1
-            
             trainer += 1
         
         return player
