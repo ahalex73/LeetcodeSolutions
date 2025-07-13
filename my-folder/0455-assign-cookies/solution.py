48915e525930bd86a90f570e158dbe48c5ceb6ee
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-07-13 14:01:56 +0000

    Sync LeetCode submission - Assign Cookies - Runtime - 16 ms (86.00%), Memory - 20 MB (13.55%)

---
+++ my-folder/0455-assign-cookies/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        # Time: O(n)
+        # Space: O(1)
+        
+        g.sort()
+        s.sort()
+
+        child = 0
+        cookie = 0
+
+        while(child < len(g) and cookie < len(s)):
+            if(g[child] <= s[cookie]):
+                child += 1
+            
+            cookie += 1
+        
+        return child
