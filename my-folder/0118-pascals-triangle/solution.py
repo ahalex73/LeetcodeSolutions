bc7f45a29c8e64631b259ff1f3ed536f4f3fdb5f
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-12 18:48:20 +0000

    Sync LeetCode submission - Pascal's Triangle - Runtime - 0 ms (100.00%), Memory - 17.8 MB (37.23%)

--- my-folder/0118-pascals-triangle/solution.py
+++ my-folder/0118-pascals-triangle/solution.py
@@ -1,22 +1,22 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        # Time: O(n^2)
+        # Time: O(n^2) 
         # Space: O(n^2)
-        # Dynamic Approach
         if numRows == 0:
             return []
+
         if numRows == 1:
             return [[1]]
 
-        prev_rows = self.generate(numRows - 1)
-        prev_row = prev_rows[-1]
-        current_row = [1]
+        # Memorize the list of prevRows
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
 
+        # Add numbers in the center
         for i in range(1, numRows - 1):
-            current_row.append(prev_row[i - 1] + prev_row[i])
+            # Calculate new Row value
+            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
 
-        current_row.append(1)
-        prev_rows.append(current_row)
-
-        return prev_rows
-
+        prevRows.append(newRow)
+        
+        return prevRows
