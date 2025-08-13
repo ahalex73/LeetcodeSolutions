971e5efaf8e250d8a57c425f404472a77911b133
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-13 19:01:35 +0000

    Sync LeetCode submission - Cells with Odd Values in a Matrix - Runtime - 0 ms (100.00%), Memory - 17.8 MB (92.09%)

--- my-folder/1378-cells-with-odd-values-in-a-matrix/solution.py
+++ my-folder/1378-cells-with-odd-values-in-a-matrix/solution.py
@@ -1,41 +1,26 @@
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-        # m = row
-        # n = col
-        # indices represents a 0-indexed location to perform some increment operations on the matrix
-        
-        """First take the indices [0,1], here 0 represent the 0th row and 1 represent the 1st column so it will first increment the values of 0th row then the inital matrix become [[1,1,1],[0,0,0]] and then it will increment the values of 1st column then the initial matrix become [[1,2,1], [0,1,0]]
-Similarly for other indices [1,1], here it will increment the values of 1st row then inital matrix become [[1,2,1], [1,2,1]] then it will increment the value of 1st column then the inital matrix become [[1,3,1], [1,3,1]]
-Now the no. of odd values are 6 in this matrix so output will be 6"""
+        # Time: O(k + (m x n)) - k = number of operations from indicies matrix
+        # Space: O(m x n)
+        # m - rows
+        # n - cols
 
-        """ Example
-            m = 2                     ->      row_increments = [1,1]
-            n = 3                     ->      col_increments = [0,2,0]
-            indices = [[0,1],[1,1]]   ->
+        row_count = [0] * m
+        col_count = [0] * n
 
-            Then check if each cell is odd
-                Cell (0,0): 1+0=1 → odd ✅
-                Cell (0,1): 1+2=3 → odd ✅
-                Cell (0,2): 1+0=1 → odd ✅
-                Cell (1,0): 1+0=1 → odd ✅
-                Cell (1,1): 1+2=3 → odd ✅
-                Cell (1,2): 1+0=1 → odd ✅
-        """
-
-        # Track how many times each row and column is incremented
-        row_increments = [0] * m
-        col_increments = [0] * n
-
+        # Get the counts of each operation per index
         for r, c in indices:
-            row_increments[r] += 1
-            col_increments[c] += 1
+            row_count[r] += 1
+            col_count[c] += 1
 
-        # Count cells that are odd
+        # Find which indices are odd or even
+                # row_count = [1, 1]
+                # col_count = [0, 2, 0]
         odd_count = 0
-        for i in range(m):
-            for j in range(n):
-                if (row_increments[i] + col_increments[j]) % 2 == 1:
-                    odd_count += 1
+
+        for r in range(m):
+            for c in range(n):
+                if((row_count[r] + col_count[c]) % 2 == 1):
+                    odd_count += 1 
 
         return odd_count
-
