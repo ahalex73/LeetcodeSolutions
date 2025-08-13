c992f29f58a38cfbf9758fab99c467a8f22d9086
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-13 23:01:48 +0000

    Sync LeetCode submission - Spiral Matrix II - Runtime - 0 ms (100.00%), Memory - 17.7 MB (70.79%)

--- my-folder/0059-spiral-matrix-ii/solution.py
+++ my-folder/0059-spiral-matrix-ii/solution.py
@@ -1,39 +1,41 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        # Time: O(m x n)
-        # Space: O(m x n)
         matrix = [[0] * n for _ in range(n)]
-        
+
         top, bottom = 0, n - 1
         left, right = 0, n - 1
 
         curr_num = 0
-        
-        while top <= bottom and left <= right:
-            # Traverse top row (left → right)
+
+        while(top <= bottom and left <= right):
+            # Traverse to the right         (left -> right)
             for col in range(left, right + 1):
                 curr_num += 1
                 matrix[top][col] = curr_num
+
             top += 1
             
-            # Traverse right column (top → bottom)
+            # Traverse to the bottom        (top -> bottom on rightside)
             for row in range(top, bottom + 1):
                 curr_num += 1
                 matrix[row][right] = curr_num
+            
             right -= 1
             
-            # Traverse bottom row (right → left)
-            if top <= bottom:
+            # Traverse to the left (if any) (right -> left on bottom)
+            if(top <= bottom):
                 for col in range(right, left - 1, -1):
                     curr_num += 1
                     matrix[bottom][col] = curr_num
-                bottom -= 1
             
-            # Traverse left column (bottom → top)
-            if left <= right:
+            bottom -= 1
+
+            # Traverse to the top  (if any) (bottom -> top on left side)
+            if (left <= right):
                 for row in range(bottom, top - 1, -1):
                     curr_num += 1
                     matrix[row][left] = curr_num
-                left += 1
-        
+
+            left += 1
+
         return matrix
