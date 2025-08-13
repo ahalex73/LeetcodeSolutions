e63b65ad69b45d64b8c6ad61634eeed1de53b388
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-13 18:07:19 +0000

    Sync LeetCode submission - Flipping an Image - Runtime - 4 ms (4.42%), Memory - 17.7 MB (60.73%)

--- my-folder/0861-flipping-an-image/solution.py
+++ my-folder/0861-flipping-an-image/solution.py
@@ -1,18 +1,23 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        # Time: O(n^2)
+        # Time: O(n^2) - however it is O(n/2) for half as many accesses using a midpoint
         # Space: O(1)
-        # NIAVE SOLUTION
+        # In place solution
 
-        # Flip the image horizonatally - reverse the array
         for row in image:
-            # Reverse the row in-place
-            row.reverse()
-            
-            # Flip each bit using XOR
-            for col in range(len(row)):
-                row[col] ^= 1
+            lp = 0
+            rp = len(row) - 1
+
+            while lp <= rp:
+                if lp == rp:
+                    row[lp] ^= 1
+
+                row[lp], row[rp] = row[rp], row[lp]
+                row[lp] ^= 1
+                row[rp] ^= 1
+
+                lp += 1
+                rp -= 1
 
         return image
-
                 
