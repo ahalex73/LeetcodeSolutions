a780c95a0bcf5c186c7922522cb5d2e3c9507e10
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-13 17:36:00 +0000

    Sync LeetCode submission - Matrix Diagonal Sum - Runtime - 0 ms (100.00%), Memory - 18.3 MB (42.16%)

---
+++ my-folder/1677-matrix-diagonal-sum/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        # Time: O(n)
+        # Space: O(1)
+        n = len(mat)
+        midpoint = 0
+        total_sum = 0
+
+        if (n % 2 == 1):
+            # It is an even nxn matrix - no midpoint
+            mid = n // 2
+            midpoint = mat[mid][mid]
+
+        for i in range(n):
+            # Primary diagonal
+            total_sum += mat[i][i] 
+
+            # Secondary diagonal
+            total_sum += mat[i][n - 1 - i]
+
+        # Remove duplicate entries if the matrix has a midpoint
+        total_sum -= midpoint
+
+        return total_sum
+
