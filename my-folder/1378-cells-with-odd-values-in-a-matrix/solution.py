823e668d42862a18240d3b8e55f7c0561604aa27
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-13 19:03:57 +0000

    Sync LeetCode submission - Cells with Odd Values in a Matrix - Runtime - 0 ms (100.00%), Memory - 17.7 MB (92.09%)

--- my-folder/1378-cells-with-odd-values-in-a-matrix/solution.py
+++ my-folder/1378-cells-with-odd-values-in-a-matrix/solution.py
@@ -1,26 +1,18 @@
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-        # Time: O(k + (m x n)) - k = number of operations from indicies matrix
-        # Space: O(m x n)
-        # m - rows
-        # n - cols
-
         row_count = [0] * m
         col_count = [0] * n
 
-        # Get the counts of each operation per index
         for r, c in indices:
             row_count[r] += 1
             col_count[c] += 1
-
-        # Find which indices are odd or even
-                # row_count = [1, 1]
-                # col_count = [0, 2, 0]
-        odd_count = 0
+        
+        odd_total = 0
 
         for r in range(m):
             for c in range(n):
                 if((row_count[r] + col_count[c]) % 2 == 1):
-                    odd_count += 1 
-
-        return odd_count
+                    # The cell is odd
+                    odd_total += 1 
+                
+        return odd_total
