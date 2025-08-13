07a6ce6b73f2327f74f13297f24e17a46bda01c1
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-13 19:19:20 +0000

    Sync LeetCode submission - Rotate Image - Runtime - 0 ms (100.00%), Memory - 17.7 MB (66.89%)

--- my-folder/0048-rotate-image/solution.py
+++ my-folder/0048-rotate-image/solution.py
@@ -5,16 +5,15 @@ class Solution:
         """
         # Time: O(n^2)
         # Space: O(1)
-        
+
         n = len(matrix)
 
         # 1. Perform a transpose
-        # 2. Perform a horizontal flip 
-
         for r in range(n):
             for c in range(r + 1, n):
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
         
+        # 2. Perform a horizontal flip 
         for row in matrix:
             row.reverse()
         
