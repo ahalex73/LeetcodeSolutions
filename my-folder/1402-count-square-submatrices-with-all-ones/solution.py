a59379e6bf29c4074ea099bddcec03c88e5bcd2f
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-20 20:17:27 +0000

    Sync LeetCode submission - Count Square Submatrices with All Ones - Runtime - 147 ms (18.53%), Memory - 29.4 MB (10.41%)

---
+++ my-folder/1402-count-square-submatrices-with-all-ones/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        
+        ROWS, COLS = len(matrix), len(matrix[0])
+        dp = defaultdict(int)
+
+        res = 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c]:
+                    dp[(r, c)] = 1 + min(
+                        dp[(r - 1, c)],
+                        dp[(r, c - 1)],
+                        dp[(r - 1, c - 1)]
+                    )
+                    res += dp[(r, c)]
+                
+        return res
+        
+        
