06020d47c9bd3fa4483666a7254265ab01c9dc1b
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-20 23:11:03 +0000

    Sync LeetCode submission - Permutations - Runtime - 0 ms (100.00%), Memory - 17.9 MB (58.31%)

---
+++ my-folder/0046-permutations/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # Time: O(n * n!)
+        # Space: O(n * n!)
+        
+        n = len(nums)
+        ans, sol = [], []
+
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol[:])
+                return 
+            
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+
+        backtrack()
+
+        return ans
