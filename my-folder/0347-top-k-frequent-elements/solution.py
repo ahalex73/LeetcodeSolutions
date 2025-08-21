9303c15f8fd71779901c73381bc67903ce977e40
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-21 02:34:23 +0000

    Sync LeetCode submission - Top K Frequent Elements - Runtime - 3 ms (90.57%), Memory - 21.3 MB (39.56%)

--- my-folder/0347-top-k-frequent-elements/solution.py
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -7,14 +7,14 @@ class Solution:
         freq = {} 
 
         for i in nums:
-            freq[i] = 1 + freq.get(i, 0)
+            freq[i] = 1 + freq.get(i, 0) # O(n)
         
         for key, val in freq.items():
-            heapq.heappush(heap, (-val, key))
+            heapq.heappush(heap, (-val, key)) # O(log n)
         
         res = []
 
         while len(res) < k:
-            res.append(heapq.heappop(heap)[1])
+            res.append(heapq.heappop(heap)[1])  # O(log n)
         
         return res
