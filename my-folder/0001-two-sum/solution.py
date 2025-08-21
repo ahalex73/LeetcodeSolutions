93b0e27e4e3a4844015c3bc4a61f702ad46d0aa8
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-21 16:49:44 +0000

    Sync LeetCode submission - Two Sum - Runtime - 0 ms (100.00%), Memory - 18.9 MB (50.24%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Time: O(n)
-        # Space: O(n)
-        
+        # Space: O(n
         pair_idx = {}
 
         for i, num in enumerate(nums):
-            if target - num in pair_idx:
+            if (target - num) in pair_idx:
                 return [i, pair_idx[target - num]]
             pair_idx[num] = i
