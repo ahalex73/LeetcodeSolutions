cf980f7eefe46fc1001105d05ebb9fea43c25bd2
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-21 17:05:15 +0000

    Sync LeetCode submission - Valid Palindrome - Runtime - 24 ms (6.09%), Memory - 18.2 MB (56.10%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -2,27 +2,24 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         # Time: O(n)
         # Space: O(1)
-
         l = 0
         r = len(s) - 1
 
-        while(l < r):
-            
-            # Skip the non-alphnumeric characters from the left pointer
-            while(l < r) and not self.isAlphaNum(s[l]):
+        while l < r:
+            # Skip alphanum on left side
+            while (l < r) and not self.isAlphaNum(s[l]):
                 l += 1
-            
-            # Skip the non-alphanumeric characters from the right pointer 
-            while(r > l) and not self.isAlphaNum(s[r]):
+
+            # Skip alphanum on right side
+            while (l < r) and not self.isAlphaNum(s[r]):
                 r -= 1
-            
-            # Verify that the opposing characters are equal to be a palindrome
-            if(s[l].lower() != s[r].lower()):
+
+            # While they are equal on each side
+            if(s[l].lower() == s[r].lower()):
+                l += 1
+                r -= 1
+            else:
                 return False
-            
-            l += 1
-            r -= 1
-        
         return True
             
     def isAlphaNum(self, c):
