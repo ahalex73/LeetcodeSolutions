58d2bd2416314a27bad2e6b06d9cf20d6e2f5bbd
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-24 14:37:40 +0000

    Sync LeetCode submission - Rotating the Box - Runtime - 130 ms (72.81%), Memory - 63.4 MB (55.77%)

--- my-folder/1972-rotating-the-box/solution.py
+++ my-folder/1972-rotating-the-box/solution.py
@@ -2,25 +2,25 @@ class Solution:
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
         # Time: O(n x m)
         # Space: O(n x m)
-        # Step 1: Rotate 90° clockwise
-        # reverse rows + transpose
-        rotated = [list(row) for row in zip(*boxGrid[::-1])] # boxGrid[::-1] is reversing row elements and then * before boxGrid is unpacking
-                                                             #               into rows, then zip is putting them into columns
+
+        rotated = [list(row) for row in zip(*boxGrid[::-1])]
         rows, cols = len(rotated), len(rotated[0])
 
-        # Step 2: Apply gravity column by column
-        for col in range(cols):
-            write = rows - 1  # bottom-most available spot
-            
-            for row in range(rows - 1, -1, -1):
-                if rotated[row][col] == '*':
-                    write = row - 1  # reset "floor" above obstacle
-                
-                elif rotated[row][col] == '#':
-                    if row != write:  # move stone down
-                        rotated[row][col] = '.'
-                        rotated[write][col] = '#'
-                    write -= 1  # next available slot is above
+        # Apply gravity - sliding window
+        for c in range(cols):
+            bot = rows - 1
+            curr_row = rows - 1
+
+            while curr_row >= 0:
+                if rotated[curr_row][c] == "*":
+                    bot = curr_row - 1
+                elif rotated[curr_row][c] == "#":
+                    if curr_row != bot:
+                        rotated[bot][c] = "#"
+                        rotated[curr_row][c] = "."
+                    bot -= 1
+
+                curr_row -= 1
 
         return rotated
 
