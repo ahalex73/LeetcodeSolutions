15bc98915aebee79d20a5587bfba034c07fd7f1e
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-24 05:41:43 +0000

    Sync LeetCode submission - Single Number - Runtime - 3 ms (64.87%), Memory - 19.7 MB (42.16%)

---
+++ my-folder/0136-single-number/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # Time: O(n)
+        # Space: O(1)
+        
+        res = 0
+
+        for n in nums:
+            res ^= n
+        
+        return res
