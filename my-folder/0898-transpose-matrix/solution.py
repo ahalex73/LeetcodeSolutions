d3b5b401d321e2dc5af65ec50d1afcac68f3ad5a
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-08-25 04:56:22 +0000

    Sync LeetCode submission - Transpose Matrix - Runtime - 0 ms (100.00%), Memory - 18.6 MB (26.01%)

--- my-folder/0898-transpose-matrix/solution.py
+++ my-folder/0898-transpose-matrix/solution.py
@@ -1,24 +1,13 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        """ In place method - ONLY VISITING THE TOP RIGHT OF THE MATRIX - SKIP diagonals
-                            # ONLY WORKS FOR SQUARE MATRIXES
-        # Time: O(n^2)
-        # Space: O(1)      
-        """
-        
-        # Time: O(n^2)
-        # Space: O(n^2)
-
         rows = len(matrix)
         cols = len(matrix[0])
 
-        transpose_matrix = []
-        
-        for c in range(cols):        
-            new_row = []
-            for r in range(rows):    
-                new_row.append(matrix[r][c])
+        res = [[0] * rows for _ in range(cols)]
 
-            transpose_matrix.append(new_row)
+        for r in range(rows):
+            for c in range(cols):
+                res[c][r] = matrix[r][c]
+            
+        return res
 
-        return transpose_matrix
